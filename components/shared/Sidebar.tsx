008c9749bb2390e6b9796de2087732921f0aa186
Shubham020303 <72245331+Shubham020303@users.noreply.github.com>
2024-04-14 14:35:30 +0000

    updated next js

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -72,7 +72,7 @@ const Sidebar = () => {
 						</ul>
 					</SignedIn>
 					<SignedOut>
-						<Button asChild className="button bg-purple-gradient bg-cover">
+						<Button asChild className="button bg-cover">
 							<Link href="/sign-in">Login</Link>
 						</Button>
 					</SignedOut>
